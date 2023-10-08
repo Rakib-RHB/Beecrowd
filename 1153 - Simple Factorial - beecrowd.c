5129aeb72deeba4199094547a0d26e05d841916e
Rakib1977 <140387583+Rakib1977@users.noreply.github.com>
2023-10-08 22:41:42 +0600

    Add files via upload

---
+++ 1153 - Simple Factorial - beecrowd.c
@@ -0,0 +1,30 @@
+
+/*Read a value N. Calculate and write its corresponding factorial. Factorial of N = N * (N-1) * (N-2) * (N-3) * ... * 1.
+
+Input
+The input contains an integer value N (0 < N < 13).
+
+Output
+The output contains an integer value corresponding to the factorial of N.
+
+Input Sample : Output Sample
+4                 24
+
+*/
+
+
+#include<stdio.h>
+int main()
+{
+    int N,i,fact ;
+    fact = 1;
+    scanf("%d",&N);
+    for(i = N; i >= 1; i-- )
+    {
+        fact = fact * i;
+    }
+    printf("%d\n",fact);
+    return 0;
+
+}
+
